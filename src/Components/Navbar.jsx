96c31fab8d1f9faf423c5a49fa1a4c419bab8e96
Sagar Vishwakarma <sv472921@gmail.com>
2024-11-10 11:01:53 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,16 +18,16 @@ const Navbar = () => {
 
                 <div className='flex flex-wrap sm:flex-col flex-row sm:h-[92vh]  items-center  justify-center font-[Roboto] font-medium'>
                     <nav className='flex sm:flex-col flex-row '>
-                        <Link to="/" className="sm:mr-4 mr-6 sm:mb-12 sm:mx-0 mx-4"><span className=' flex items-center sm:flex-row flex-col gap-x-2 text-[18px] font-bold'> <FaHome className='text-[22px]' />Home</span></Link>
+                        <Link to="/" className="sm:mr-4 mr-2 sm:mb-12 sm:mx-0 mx-4"><span className=' flex items-center sm:flex-row flex-col gap-x-2 text-[18px] font-bold'> <FaHome className='text-[22px]' />Home</span></Link>
 
 
-                        <Link to="/about" className="sm:mr-4 mr-6 sm:mb-12 sm:mx-0 mx-4"><span  className=' flex  items-center  gap-x-2 text-[18px] font-bold sm:flex-row flex-col'> <FaUser className='text-[22px]'/> About</span></Link>
+                        <Link to="/about" className="sm:mr-4 mr-2 sm:mb-12 sm:mx-0 mx-4"><span  className=' flex  items-center  gap-x-2 text-[18px] font-bold sm:flex-row flex-col'> <FaUser className='text-[22px]'/> About</span></Link>
 
 
-                        <Link to="/skills" className="sm:mr-4 mr-6 sm:mb-12 sm:mx-0 mx-4"><span  className=' flex items-center  gap-x-2 text-[18px] font-bold sm:flex-row flex-col'><FaTools className='text-[22px]'/> Skills</span></Link>
+                        <Link to="/skills" className="sm:mr-4 mr-2 sm:mb-12 sm:mx-0 mx-4"><span  className=' flex items-center  gap-x-2 text-[18px] font-bold sm:flex-row flex-col'><FaTools className='text-[22px]'/> Skills</span></Link>
 
 
-                        <Link to="/projects" className="sm:mr-4 mr-6 sm:mb-12 mb-0 sm:mx-0 mx-4"><span  className=' flex items-center  gap-x-2 text-[18px] font-bold sm:flex-row flex-col'> <FaProjectDiagram className='text-[22px]'/>Projects</span></Link>
+                        <Link to="/projects" className="sm:mr-4 mr-2 sm:mb-12 mb-0 sm:mx-0 mx-4"><span  className=' flex items-center  gap-x-2 text-[18px] font-bold sm:flex-row flex-col'> <FaProjectDiagram className='text-[22px]'/>Projects</span></Link>
                     </nav>
 
                 </div>
